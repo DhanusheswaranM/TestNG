13c732d4a2c2dafe048b1c6c95a1483ea705928b
DhanusheswaranM <mdhanusheswaran@gmail.com>
2025-03-20 22:11:05 +0530

    20-03-2025 commit

---
+++ testng/src/test/java/com/test/basics/Demo_Test.java
@@ -0,0 +1,33 @@
+package com.test.basics;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+import java.time.Duration;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo_Test {
+	public WebDriver dvr ;
+  @Test
+  public void verifyTitle() {
+	  String expectedtitle = "yahoo";
+	  String actualtitle = dvr.getTitle();
+	  Assert.assertEquals(actualtitle, expectedtitle,"Title is not same");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	dvr = new ChromeDriver();
+	dvr.manage().window().maximize();
+	dvr.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	dvr.get("https://www.google.co.in/");
+  }
+
+  @AfterTest
+  public void teardown() {
+	  dvr.quit();
+  }
+
+}
---
+++ testng/src/test/java/com/test/basics/tutorialsNinjas.java
@@ -0,0 +1,54 @@
+package com.test.basics;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import org.testng.annotations.BeforeTest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class tutorialsNinjas {
+	public WebDriver dvr ;
+  @Test
+  public void navigate() {
+	  dvr.findElement(By.xpath("//nav[@id='top']//div//div[2]//ul//li[2]//a//span[2]")).click();
+	  dvr.findElement(By.xpath("//a[contains(text(),'Register')]")).click();
+	  
+	 
+	   
+  }
+  @Test
+  public void verify() {
+	  SoftAssert soft = new SoftAssert();
+	  String str1 = "Register Accounts";
+	  String str2 = dvr.findElement(By.xpath("//h1[contains(text(),'Register Account')]")).getText();
+//	  Assert.assertEquals(str1, str2);
+	  soft.assertEquals(str2, str1, "Its wrong");
+	  
+	  dvr.findElement(By.xpath("//a[contains(text(),'Qafox.com')]")).click();
+	  
+	  String str3 = "Your Store";
+	  String str4 = dvr.getTitle();
+	  
+	  Assert.assertEquals(str3 , str4);
+	  soft.assertAll();
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  dvr = new ChromeDriver();
+	  dvr.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	  dvr.manage().window().maximize();
+	  dvr.get("https://tutorialsninja.com/demo/");
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
